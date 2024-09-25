11aa102ca201d7c1c96d1eb1ea6cab1bb5e8b217
Jmrx <jairenriquez30@gmail.com>
2024-09-25 16:44:53 -0500

    add

--- static/js/button.js
+++ static/js/button.js
@@ -1,29 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const togglePassword = document.getElementById("togglePassword");
-    const passwordInput = document.getElementById("password");
-
-    togglePassword.addEventListener("click", function() {
-        const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
-        passwordInput.setAttribute("type", type);
-        this.textContent = type === "password" ? "👀" : "🙈"; // Cambiar el ícono
-    });
-
-    const toggleConfirmation = document.getElementById("toggleConfirmation");
-    const confirmationInput = document.getElementById("confirmation");
-
-    toggleConfirmation.addEventListener("click", function() {
-        const type = confirmationInput.getAttribute("type") === "password" ? "text" : "password";
-        confirmationInput.setAttribute("type", type);
-        this.textContent = type === "password" ? "👀" : "🙈"; // Cambiar el ícono
-    });
-});
-
-/*const togglePassword = document.getElementById('togglePassword');
+const togglePassword = document.getElementById('togglePassword');
 const passwordInput = document.getElementById('password');
-const eyeIcon = document.getElementById('eyeIcon');
 
 togglePassword.addEventListener('click', function () {
-    // Cambia el tipo de input entre 'password' y 'text'
-    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-    passwordInput.setAttribute('type', type);
-});*/
+  // Toggle the type attribute
+  const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+  passwordInput.setAttribute('type', type);
+});
+
+const toggleConfirmation = document.getElementById('toggleConfirmation');
+const confirmationInput = document.getElementById('confirmation');
+
+toggleConfirmation.addEventListener('click', function () {
+  // Toggle the type attribute
+  const type = confirmationInput.getAttribute('type') === 'password' ? 'text' : 'password';
+  confirmationInput.setAttribute('type', type);
+});
