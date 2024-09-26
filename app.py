1b64481c8d26e3a86182c4a0a434d223bcbf3d79
Jmrx <jairenriquez30@gmail.com>
2024-09-26 14:36:38 -0500

    add

--- app.py
+++ app.py
@@ -78,28 +78,22 @@ def add_data():
         'hora_salida': request.form.get('hora_salida'),
         'observaciones': request.form.get('observaciones')
     }
-
     try:
         # Get form data
         fecha_registro = datetime.now().strftime('%Y-%m-%d')
-        
         # Form fields validation
         if not all(form_data.values()):
             flash("Todos los campos son obligatorios.", category='warning')
             return render_template('index.html', form_data=form_data)
-        
         if not re.match(r"^[A-Za-z\s]+$", form_data['nombre_docente']):
             flash("El nombre del docente solo puede contener letras y espacios.", category='warning')
             return render_template('index.html', form_data=form_data)
-        
         if not re.match(r"^[^,\s]+@(gmail\.com|unicesmag\.edu\.co)$", form_data['correo_electronico']):
             flash("Correo electrónico no válido. Debe ser un correo de Gmail o de la universidad.", category='warning')
             return render_template('index.html', form_data=form_data)
-        
         if form_data['hora_ingreso'] >= form_data['hora_salida']:
             flash("La hora de salida debe ser después de la hora de ingreso.", category='warning')
             return render_template('index.html', form_data=form_data)
-        
         # Save data to database
         cursor = cnx.cursor()
         query = """
@@ -110,7 +104,6 @@ def add_data():
         cnx.commit()
         flash("Datos guardados exitosamente.", category='success')
         return redirect(url_for('index'))
-
     except Exception as e:
         flash(f"Error: {e}", category='danger')
         return render_template('index.html', form_data=form_data)
@@ -123,24 +116,20 @@ def login_admin():
     if request.method == "POST": 
         input_username = request.form.get("username") 
         input_password = request.form.get("password") 
-
         # Validate field input
         if not input_username:
             return render_template("login.html", messager=1) 
         elif not input_password:
             return render_template("login.html", messager=2) 
-        
         # Create cursor to return dictionary 
         cursor = cnx.cursor(dictionary=True)
         cursor.execute('SELECT * FROM users WHERE username = %s', (input_username,))
         user = cursor.fetchone()
-
         # Validate credencials user
         if user is None or not check_password_hash(user["hash"], input_password):
             return render_template("login.html", messager=3)
         session["user_id"] = user["id"] 
         return redirect("/admin") 
-
     return render_template("/login.html")
 
 @app.route("/facereg", methods=["GET", "POST"])
@@ -150,29 +139,23 @@ def facereg():
     if request.method == "POST":
         encoded_image = (request.form.get("pic") + "==").encode('utf-8')
         username = request.form.get("name")
-        
         # Create cursor database
         cursor = cnx.cursor(dictionary=True)
         cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
         user = cursor.fetchone()
-        
         # If the user is not found. Show error message
         if not user:
             flash("⚠️ Usuario no encontrado. Por favor, comuníquese con el admistrador.", category="warning")
             return render_template("camera.html")
-        
         # Get the id of the found user
         id_ = user['id']
         compressed_data = zlib.compress(encoded_image, 9)
         uncompressed_data = zlib.decompress(compressed_data)
         decoded_data = b64decode(uncompressed_data)
-        
         dir_path = './static/face/unknown/'
         file_path = os.path.join(dir_path, f'{id_}.jpg')
-        
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
-        
         # Upload unknown images for facial recognition
         try:
             with open(file_path, 'wb') as new_image_handle:
@@ -180,7 +163,6 @@ def facereg():
         except IOError:
             flash("❌ Error al guardar la imagen. Por favor, intenta nuevamente.", category="danger")
             return render_template("camera.html")
-        
         try:
             registered_image = face_recognition.load_image_file(f'./static/face/{id_}.jpg')
             registered_face_encodings = face_recognition.face_encodings(registered_image)
@@ -191,7 +173,6 @@ def facereg():
         except FileNotFoundError:
             flash("❌ Usuario incorrecto. Por favor, vuelve a seleccionar tu nombre de usuario.", category="danger")
             return render_template("camera.html")
-        
         try:
             unknown_image = face_recognition.load_image_file(file_path)
             unknown_face_encodings = face_recognition.face_encodings(unknown_image)
@@ -202,7 +183,6 @@ def facereg():
         except FileNotFoundError:
             flash("❌ Error al cargar la imagen. Asegúrate de que la imagen se haya guardado correctamente.", category="danger")
             return render_template("camera.html")
-        
         # Compare face with a precese threshold
         face_distances = face_recognition.face_distance([registered_face_encoding], unknown_face_encoding)
         if len(face_distances) > 0 and face_distances[0] < 0.4:  
@@ -211,7 +191,6 @@ def facereg():
         else:
             flash("🚫 Rostro incorrecto. Acceso denegado.", category="danger")
             return render_template("camera.html")
-
     return render_template("camera.html")
 
 @app.route("/goback")
@@ -250,7 +229,6 @@ def update_data(id):
         hora_salida = request.form['hora_salida']
         observaciones = request.form['observaciones']
         respuesta_incidencias = request.form['respuesta_incidencias']
-
         # Update data in database
         cursor = cnx.cursor()
         query = """
@@ -289,7 +267,6 @@ def register():
         input_username = request.form.get("username")
         input_password = request.form.get("password")
         input_confirmation = request.form.get("confirmation")
-    
         # Validate data user
         if not input_username:
             return render_template("register.html", messager=1)
@@ -299,23 +276,20 @@ def register():
             return render_template("register.html", messager=4)
         elif input_password != input_confirmation:
             return render_template("register.html", messager=3)
-        
+        # Create cursor database
         cursor = cnx.cursor()
         cursor.execute('SELECT username FROM users WHERE username = %s', (input_username,))
         if cursor.fetchone():
             return render_template("register.html", messager=5)
-            
         # Generate hashed password
         hash_password = generate_password_hash(input_password, method='pbkdf2:sha256', salt_length=8)
         cursor.execute('INSERT INTO users (username, hash) VALUES (%s, %s)', (input_username, hash_password))
         cnx.commit()
         new_user_id = cursor.lastrowid
-
         # Saving id user
         session["user_id"] = new_user_id
         flash(f"Usuario registrado como {input_username}")
         return redirect("/admin")
-
     return render_template("register.html")
 
 @app.route("/users")
@@ -333,24 +307,19 @@ def facesetup():
     if request.method == "POST":
         # Get and encode the image
         encoded_image = (request.form.get("pic") + "==").encode('utf-8')
-        
+        # Create cursor database
         cursor = cnx.cursor(dictionary=True)
         cursor.execute("SELECT id FROM users WHERE id = %s", (session["user_id"],))
         user = cursor.fetchone()
-        
         # If the user is not found, displat an error message
         if not user:
             flash("⚠️ Usuario no encontrado. Por favor, inicia sesión nuevamente.", category="danger")
             return render_template("face.html")
-        
         id_ = user["id"]
         compressed_data = zlib.compress(encoded_image, 9)
         uncompressed_data = zlib.decompress(compressed_data)
         decoded_data = b64decode(uncompressed_data)
-        
         file_path = f'./static/face/{id_}.jpg'
-        
-
         # Upload image for facial recognition
         try:
             with open(file_path, 'wb') as new_image_handle:
@@ -358,7 +327,6 @@ def facesetup():
         except IOError:
             flash("❌ Error al guardar la imagen. Por favor, intenta nuevamente.", category="danger")
             return render_template("face.html")
-        
         try:
             image = face_recognition.load_image_file(file_path)
             face_encodings = face_recognition.face_encodings(image)
@@ -369,29 +337,23 @@ def facesetup():
         except Exception as e:
             flash("⚠️ Error al procesar la imagen. Por favor, intenta nuevamente.")
             return render_template("face.html")
-        
         return redirect("/users")
-    
     return render_template("face.html")
 
 @app.route("/restore/<id>", methods=['GET', 'POST'])
 @login_required
 def restore(id):
     cursor = cnx.cursor()
-    
     # Get form restore
     if request.method == 'POST':
         new_password = request.form.get('password')
         confirmation = request.form.get('confirmation')
-        
         # Initialize message variable
         message_code = 0
-
         # Get the current username from the database to keep it unchanged
         cursor.execute("SELECT username FROM users WHERE id = %s", (id,))
         user = cursor.fetchone()
         current_username = user[0] if user else ''
-
         # Validate passwords
         if not new_password:
             message_code = 2  # Password is empty
@@ -401,21 +363,17 @@ def restore(id):
             # Generate hash for new password
             hash_password = generate_password_hash(new_password, method='pbkdf2:sha256', salt_length=8)
             cursor.execute("UPDATE users SET hash = %s WHERE id = %s", (hash_password, id))
-
         # Commit changes only if there are no validation errors
         if message_code == 0:
             cnx.commit()
             flash("Datos actualizados exitosamente.")
             return redirect("/users")
-
         # Render template with appropriate message and current username
         return render_template("restore.html", messager=message_code, id=id, username=current_username)
-
     # Get data user form
     cursor.execute("SELECT username FROM users WHERE id = %s", (id,))
     user = cursor.fetchone()
     username = user[0] if user else ''
-
     return render_template("restore.html", messager=0, id=id, username=username)
 
 @app.route("/deleteuser/<id>")
