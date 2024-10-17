4af01af38fac5411124bd1058b2816e4e18768b7
Jmrxr <jairenriquez30@gmail.com>
2024-10-17 08:44:58 -0500

    add

--- static/js/button.js
+++ static/js/button.js
@@ -2,7 +2,6 @@ const togglePassword = document.getElementById('togglePassword');
 const passwordInput = document.getElementById('password');
 
 togglePassword.addEventListener('click', function () {
-  // Toggle the type attribute
   const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
   passwordInput.setAttribute('type', type);
 });
@@ -11,7 +10,6 @@ const toggleConfirmation = document.getElementById('toggleConfirmation');
 const confirmationInput = document.getElementById('confirmation');
 
 toggleConfirmation.addEventListener('click', function () {
-  // Toggle the type attribute
   const type = confirmationInput.getAttribute('type') === 'password' ? 'text' : 'password';
   confirmationInput.setAttribute('type', type);
 });
